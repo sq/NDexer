c4c20571df033936c3a6c9cbf706c7551376ae20
Varun Khaneja <aawc@googlegroups.com>
2013-07-16 00:58:14 -0700

    Adding support for Sublime Text
    Seems to work with Sublime Text 3.

--- TextEditors/Director.cs
+++ TextEditors/Director.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.Runtime.InteropServices;
-using Squared.Task;
+﻿using Squared.Task;
+using System;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
 namespace Ndexer {
 #if !MONO
---
+++ TextEditors/SublimeText.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Ndexer {
+#if !MONO
+    public class SublimeTextDirector : Director, IBasicDirector {
+        public SublimeTextDirector (string applicationPath)
+            : base(applicationPath) {
+        }
+
+        protected override IntPtr FindDirectorWindow () {
+            return IntPtr.Zero;
+        }
+
+        protected override IntPtr FindEditorWindow () {
+            return FindWindow("PX_WINDOW_CLASS", null);
+        }
+
+        public void Launch (string arguments) {
+            var info = new ProcessStartInfo(_ApplicationPath, arguments);
+            var process = Process.Start(info);
+            process.WaitForInputIdle();
+            _EditorWindow = FindEditorWindow();
+            process.Dispose();
+        }
+
+        public void OpenFile (string filename) {
+            Launch(filename);
+        }
+
+        public void OpenFile (string filename, long initialLineNumber) {
+			Launch(String.Format("\"{0}\":{1:0}", filename, initialLineNumber));
+        }
+
+        public static bool LocateExecutable (ref string filename) {
+			return false;
+        }
+    }
+#endif
+}
